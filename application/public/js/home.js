9f6c1c0708b7b595e526aa0a6c7a3585b7177cef
Burmy <62005549+Burmy@users.noreply.github.com>
2021-04-19 20:57:12 -0700

    minor changes

--- application/public/js/home.js
+++ application/public/js/home.js
@@ -35,3 +35,11 @@ function fadeOut(id) {
         opacity -= 0.1;
     }, 20);
 }
+
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+    mainNav.classList.toggle('active');
+});
