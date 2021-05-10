23d5224f86ecfa195de9cff0890d5a42dba5ebc4
Burmy <62005549+Burmy@users.noreply.github.com>
2021-05-10 16:47:10 -0700

    Adding Multer and Sharp to upload files

--- application/app.js
+++ application/app.js
@@ -9,6 +9,7 @@ var flash = require('express-flash')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
 var dbRouter = require('./routes/dbtest');
 var errorPrint = require('./helpers/debug/debugprinters').errorPrint;
 var requestPrint = require('./helpers/debug/debugprinters').requestPrint;
@@ -65,6 +66,7 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 app.use('/dbtest', dbRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 
 app.use((err, req, res, next) => {
