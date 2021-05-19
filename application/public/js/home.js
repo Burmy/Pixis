66595720477fed7505c6b8afdf300e415d29f7ce
Burmy <62005549+Burmy@users.noreply.github.com>
2021-05-19 14:54:47 -0700

    minor updates

--- application/public/js/home.js
+++ application/public/js/home.js
@@ -1,10 +1,3 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function () {
-    mainNav.classList.toggle('active');
-});
-
 function setFlashMsgFadeOut(flashMessageElement) {
     setTimeout(() => {
         let currentOpacity = 1.0;
